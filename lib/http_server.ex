f1d9b2b0506714f217aabcf97de3817f182d6e64
Tom Brunoli <tominated@gmail.com>
2013-02-02 23:48:29 +1000

    Changed route return to tuple to allow for errors. Basic file serving working - works, but comes up with error logs

--- lib/http_server.ex
+++ lib/http_server.ex
@@ -47,14 +47,11 @@ defmodule HttpServer do
   def handle_request(caller, socket) do
     request = get_request_data(socket)
     data = caller.route(request.method, request.path, request)
-    response = """
-    HTTP/1.1 200 OK
-    Host: localhost
-    Content-Type: text/html
-    Content-Length: #{String.length data}
-    
-    #{data}
-    """
+    response = case data do
+      { :ok, html } -> generate_response html
+      { :ok, file, :file } -> file
+      e -> generate_error e
+    end
     TCP.send(socket, response)
     TCP.close(socket)
   end
@@ -111,7 +108,33 @@ defmodule HttpServer do
     end
   end
   
-  defmacro __using__(_opts) do
+  def generate_response({ :ok, html }) do
+    """
+    HTTP/1.1 200 OK
+    Host: localhost
+    Content-Type: text/html
+    Content-Length: #{String.length html}
+    
+    #{html}
+    """
+  end
+  
+  def generate_error({ :error, error_code }) do
+    error_type = case error_code do
+      400 -> "400 Bad Request"
+      404 -> "404 Not Found"
+      _ -> "503 Internal Server Error"
+    end
+    
+    """
+    HTTP/1.1 #{error_type}
+    Host: localhost
+    """
+  end
+  
+  defmacro __using__(opts) do
+    root_val = Keyword.get(opts, :root, ".")
+    
     quote do
       import HttpServer, only: [all: 1, all: 2, get: 2, post: 2]
   
@@ -121,6 +144,19 @@ defmodule HttpServer do
       
       def start(port) do
         HttpServer.start unquote(__CALLER__.module), port
+      end
+      
+      defp static_root, do: unquote(root_val)
+      
+      def route(:GET, ["public" | path], _request) do
+        full_path = Path.join([static_root()] ++ path)
+        case File.read(full_path) do
+          { :ok, data } ->
+            { :ok, data, :file }
+          e ->
+            IO.inspect e
+            { :error, 404 }
+        end
       end
     end
   end
--- lib/web_app.ex
+++ lib/web_app.ex
@@ -1,16 +1,20 @@
 defmodule WebApp do
-  use HttpServer
+  use HttpServer, root: "public"
   
   get ["test", id] do
-    """
+    html = """
     <p>Test. ID: #{id}</p>
     """
+    
+    { :ok, html }
   end
   
   all do
-    """
+    html = """
     <h1>Hello World!</h1>
     <pre>#{inspect request}</pre>
     """
+    
+    { :ok, html }
   end
 end
